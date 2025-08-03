3b4c3756d1893e71085d9f6645399c7a32d9bc5d
Patrycja <patrycja.nazim@gmail.com>
2025-08-03 22:34:14 +0200

    added map2, on click mechanics, mouse hovering mechanics

--- Assets/Scripts/MapState2.cs
+++ Assets/Scripts/MapState2.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using UnityEngine;
-public class MapState : GameState
-    {
+public class MapState2 : GameState
+{
     public override void Enter(SceneController controller)
     {
-        controller.LoadScene("Map_2");
-        controller.ChangeState(new MapState2());
+
+        // this will solve Dijkstra Algorithm
     }
 
     public override void Exit(SceneController controller)
     {
         controller.LoadScene("SampleScene");
         controller.ChangeState(new DuringGameState());
-        
+
     }
 
     public override void KeyboardInput(SceneController controller)
     {
         controller.LoadScene("Inventory");
         controller.ChangeState(new InventoryState());
-        
+
     }
 
 }
+
 
--- Assets/Scripts/NodeController.cs
+++ Assets/Scripts/NodeController.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using static UnityEngine.GraphicsBuffer;
 
 public class NodeController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static NodeController instance;
     public List<GameObject> circles = new List<GameObject>();
+    private GameObject lastHovered;
+    private Color originalColor;
+
+
     void Start()
     {
         GameObject[] foundCircles = GameObject.FindGameObjectsWithTag("Node");
@@ -22,15 +28,43 @@ public class NodeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.enterKey.wasPressedThisFrame)
+
+        Vector2 mouseWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Collider2D hit = Physics2D.OverlapPoint(mouseWorld);
+
+        if (hit != null && hit.CompareTag("Node"))
         {
-            foreach (GameObject circle in circles)
+            GameObject current = hit.gameObject;
+
+            if (current != lastHovered)
             {
-                circle.GetComponent<SpriteRenderer>().color = Color.green;
+                // Reset color of previous hovered object
+                ResetLastHovered();
+
+                // Highlight new one
+                SpriteRenderer sr = current.GetComponent<SpriteRenderer>();
+                if (sr != null)
+                {
+                    originalColor = sr.color;
+                    sr.color = Color.yellow;
+                    lastHovered = current;
+                }
+            }
+
+
+        }
+    }
+        void ResetLastHovered()
+        {
+            if (lastHovered != null)
+            {
+                SpriteRenderer sr = lastHovered.GetComponent<SpriteRenderer>();
+                if (sr != null)
+                    sr.color = Color.red;
+
+                lastHovered = null;
             }
         }
-
-       
     }
-  
-}
+
+
